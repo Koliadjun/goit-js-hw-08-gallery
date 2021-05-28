d2852a75b1508074359760ed120ad79a519a65ed
koliadjun <koliadjun@gmail.com>
2021-05-28 15:13:54 +0300

    swipe implemented

--- script.js
+++ script.js
@@ -97,6 +97,53 @@ function lazyLoadSupport() {
     }
 }
 
+refs.lightboxImageEl.addEventListener('touchstart', handleTouchStart, false);
+refs.lightboxImageEl.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {/*most significant*/
+        if (xDiff > 0) {
+            setLightBoxImageSrcAttribute(nextImgUrl(currentImgSourceUrl));
+        } else {
+            setLightBoxImageSrcAttribute(prevImgUrl(currentImgSourceUrl));
+        }
+    } else {
+        if (yDiff > 0) {
+            modalOpenClose(ACTION.close);
+        } else {
+            /* down swipe */
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
+
+
 if (refs.input.checked) {
     inputData = pixabayCovertData(data);
 } else { inputData = source.default; }
